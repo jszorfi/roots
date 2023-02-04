7083ee252fc76a2c334000296638572d30f92ae0
jszorfi <44711471+jszorfi@users.noreply.github.com>
2023-02-04 13:42:24 +0100

    CanvasController

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -3,17 +3,41 @@ using UnityEngine;
 public class CanvasController : MonoBehaviour
 {
     private CanvasGroup potatoSkills;
+    private CanvasGroup carrotSkills;
+    private CanvasGroup radishSkills;
+    private CanvasGroup builderSkills;
+
+    private CanvasGroup displayedSkills;
     private void Start()
     {
         potatoSkills = gameObject.transform.GetChild(0).gameObject.GetComponent<CanvasGroup>();
+        carrotSkills = gameObject.transform.GetChild(0).gameObject.GetComponent<CanvasGroup>();
+        radishSkills = gameObject.transform.GetChild(0).gameObject.GetComponent<CanvasGroup>();
+        builderSkills = gameObject.transform.GetChild(0).gameObject.GetComponent<CanvasGroup>();
+    }
+
+    public void ChangeDisplayedSkills(CanvasGroup skillsToDisplay)
+    {
+        if (displayedSkills != null)
+            displayedSkills.alpha = 0;
+        skillsToDisplay.alpha = 1;
+        displayedSkills = skillsToDisplay;
     }
 
     public void DisplayPotatoSkills()
     {
-        if (potatoSkills.alpha == 1)
-            potatoSkills.alpha = 0;
-        else if (potatoSkills.alpha == 0)
-            potatoSkills.alpha = 1;
+        ChangeDisplayedSkills(potatoSkills);
     }
-
+    public void DisplayCarrotSkills()
+    {
+        ChangeDisplayedSkills(carrotSkills);
+    }
+    public void DisplayRadishSkills()
+    {
+        ChangeDisplayedSkills(radishSkills);
+    }
+    public void DisplayBuilderSkills()
+    {
+        ChangeDisplayedSkills(builderSkills);
+    }
 }
