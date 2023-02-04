66acdf453fd69ebe6fce90ca36b07c15b715811c
Peter Gyulai <peter.gyulai@pcbdesign.hu>
2023-02-04 19:19:12 +0100

    Added nice camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,53 +2,51 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool downThisFrame  = false;
-    private float speed         = 0.03f;
-    private Vector3 dir;
-    private readonly Vector3 up    = new Vector3( 0,  1,  0);
-    private readonly Vector3 down  = new Vector3( 0, -1,  0);
-    private readonly Vector3 left  = new Vector3(-1,  0,  0);
-    private readonly Vector3 right = new Vector3( 1,  0,  0);
+    // The movement speed of the camera
+    public float cameraSpeed;
+
+    // The smoothness of the camera movement
+    public float smoothness;
+
+    // Internal state of the camera controller
+    private Camera m_Camera;
+    private Vector3 m_TargetPosition;
+    private Vector3 m_CameraVelocity;
+
+    void Start()
+    {
+        m_Camera = Camera.main;
+        m_TargetPosition = m_Camera.transform.position;
+    }
 
     void FixedUpdate()
     {
+        float heightFactor = 1.0f; //transform.position.z / 3.0f;
 
-        if (Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
-            dir = up;
-            downThisFrame = true;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            dir = left;
-            downThisFrame = true;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            dir = down;
-            downThisFrame = true;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            
-            dir = right;
-            downThisFrame = true;
+            Debug.Log("Before set: " + m_TargetPosition);
+            m_TargetPosition += new Vector3(0.0f, 1.0f, 0.0f) * cameraSpeed * heightFactor;
+            Debug.Log("After set: " + m_TargetPosition);
         }
 
-        if (downThisFrame)
+        if(Input.GetKey(KeyCode.S))
         {
-            transform.Translate(speed*dir, Space.World);
-
-            if(speed < 0.7f)
-            {
-                speed += 0.01f;
-            }
-        }
-        else
-        {
-            speed = 0.03f;
+            m_TargetPosition += new Vector3(0.0f, 1.0f, 0.0f) * -cameraSpeed * heightFactor;
         }
 
-        downThisFrame = false;
+        if(Input.GetKey(KeyCode.A))
+        {
+            m_TargetPosition += new Vector3(1.0f, 0.0f, 0.0f) * -cameraSpeed * heightFactor;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            m_TargetPosition += new Vector3(1.0f, 0.0f, 0.0f) * cameraSpeed * heightFactor;
+        }
+
+        //Debug.Log("Before move: " + m_Camera.transform.position);
+        m_Camera.transform.position = Vector3.SmoothDamp(m_Camera.transform.position, m_TargetPosition, ref m_CameraVelocity, smoothness);     
+        //Debug.Log("After move: " + m_Camera.transform.position);
     }
 }
